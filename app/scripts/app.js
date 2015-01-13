c0eee4aa259b6a33d7bd3e7f2c6e926a8b11dcf0
javigonz <javier.gonzalez@gmail.com>
2015-01-13 09:34:30 +0100

    commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,9 +7,15 @@
       .config(config)
 
 
-  function config ($locationProvider, $routeProvider) {
+  function config ($locationProvider, $routeProvider, $controllerProvider) {
 
-    $locationProvider.html5Mode(true);
+    //With angular 1.3 you can no longer use global controller declaration on the global scope. You would need to define the controller using inline declaration.
+    $controllerProvider.allowGlobals(); 
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
 
     $routeProvider
         .when('/', {
